bbb9993d6fad93e4eb5a73ad311feb2bc7115bf7
duarteicaros <icaroduartec@gmail.com>
2021-07-24 20:17:12 -0300

    Atualização DB pra fake api - Netlify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://my-json-server.typicode.com/duarteicaros/VTEX-Desafio2/produtos-F1/'
+    baseURL: 'http://my-json-server.typicode.com/duarteicaros/db-vtex/produtos-F1'
 })
 
 export default api;
